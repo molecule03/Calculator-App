c61d044fb4104386968e00af55176dfbfbdb5781
molecule03 <laveshsalaskar619@gmail.com>
2023-07-10 13:25:22 +0530

    Documentation Added

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -13,133 +13,134 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    var lastNum : Boolean = false;
-    var lastDot : Boolean = false;
+    // Boolean variables to keep track of the last input
+    var lastNum: Boolean = false
+    var lastDot: Boolean = false
 
-    fun OnDigit(view : View){
-
-        val tvOutput : TextView = findViewById(R.id.tvOutput)
+    /**
+     * Appends the clicked digit to the output TextView and sets lastNum to true.
+     */
+    fun OnDigit(view: View) {
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
         tvOutput.append((view as Button).text)
-
-//        If click on num lastNum will be become true
-        lastNum = true;
+        lastNum = true
     }
 
-    fun OnClear(view : View){
-
-        val tvOutput : TextView = findViewById(R.id.tvOutput)
-        tvOutput.text = "";
-
-//        Screen Cleared so lastNum & lastDot will become false
+    /**
+     * Clears the output TextView and resets lastNum and lastDot to false.
+     */
+    fun OnClear(view: View) {
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
+        tvOutput.text = ""
         lastNum = false
         lastDot = false
     }
 
-    fun OnDecimal(view : View){
-
-        if(lastNum && !lastDot){
-            val tvOutput : TextView = findViewById(R.id.tvOutput)
+    /**
+     * Appends the decimal point to the output TextView if lastNum is true and lastDot is false.
+     * Then, it sets lastNum to false and lastDot to true.
+     */
+    fun OnDecimal(view: View) {
+        if (lastNum && !lastDot) {
+            val tvOutput: TextView = findViewById(R.id.tvOutput)
             tvOutput.append((view as Button).text)
-
-//            Click and do to lastNum will become false and lastDot will become true;
-            lastNum = false;
-            lastDot = true;
+            lastNum = false
+            lastDot = true
         }
     }
 
-    fun OnOperator(view : View){
-
-        val tvOutput : TextView = findViewById(R.id.tvOutput);
-        if(lastNum && !isOperatorAdded(tvOutput.text.toString())) {
+    /**
+     * Appends the clicked operator to the output TextView if lastNum is true and an operator is not already added.
+     * Then, it sets lastNum and lastDot to false.
+     */
+    fun OnOperator(view: View) {
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
+        if (lastNum && !isOperatorAdded(tvOutput.text.toString())) {
             tvOutput.append((view as Button).text)
-
-//            Click on operator so both will become false
-            lastNum = false;
-            lastDot = false;
+            lastNum = false
+            lastDot = false
         }
     }
 
+    /**
+     * Performs the arithmetic calculation based on the expression in the output TextView.
+     * It handles addition, subtraction, multiplication, and division.
+     * The result is displayed in the output TextView.
+     */
+    fun OnEquals(view: View) {
+        val tvOutput: TextView = findViewById(R.id.tvOutput)
 
-    fun OnEquals(view : View){
-
-        val tvOutput : TextView = findViewById(R.id.tvOutput);
-
-//        Important to check is the last char is Number(For eg: 619+ -> this will give error)
-            if (lastNum) {
-                var tvValue = tvOutput.text.toString()
-                var prefix = "";
-                try {
-//                    Checking if the expression is starting with minus
-                    if (tvValue[0] == '-'){
-                        prefix="-"
-                        tvValue = tvValue.substring(1);
-                    }
-//                    Calculation for subtraction
-                    if (tvValue.contains("-")) {
-                        var splitValue = tvValue.split("-")
-
-                        var fir = splitValue[0].toDouble();
-                        var sec = splitValue[1].toDouble();
-
-                        if(prefix == "-") fir *= -1;
-                        var ans = (fir - sec);
-                        tvOutput.text = removeZeros(ans);
-                    }
-//                    Calculation for Addition
-                    else if (tvValue.contains("+")) {
-                        var splitValue = tvValue.split("+")
-
-                        var fir = splitValue[0].toDouble();
-                        var sec = splitValue[1].toDouble();
-
-                        if(prefix == "-") fir *= -1;
-                        var ans = (fir + sec);
-                        tvOutput.text = removeZeros(ans);
-                    }
-//                    Calculation for Multiplication
-                    else if (tvValue.contains("*")) {
-                        var splitValue = tvValue.split("*")
-
-                        var fir = splitValue[0].toDouble();
-                        var sec = splitValue[1].toDouble();
-
-                        if(prefix == "-") fir *= -1;
-                        var ans = (fir * sec);
-                        tvOutput.text = removeZeros(ans);
-                    }
-//                    Calculation for Division
-                    else if (tvValue.contains("/")) {
-                        var splitValue = tvValue.split("/")
-
-                        var fir = splitValue[0].toDouble();
-                        var sec = splitValue[1].toDouble();
-
-                        if(prefix == "-") fir *= -1;
-                        var ans = (fir / sec);
-                        tvOutput.text = removeZeros(ans);
-                    }
-
+        // Check if the last character is a number to avoid errors
+        if (lastNum) {
+            var tvValue = tvOutput.text.toString()
+            var prefix = ""
+            try {
+                // Checking if the expression starts with a minus sign
+                if (tvValue[0] == '-') {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
                 }
-                catch(e : ArithmeticException){
-                    e.printStackTrace()
-                };
-            }
 
+                // Calculation for subtraction
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var fir = splitValue[0].toDouble()
+                    var sec = splitValue[1].toDouble()
+
+                    if (prefix == "-") fir *= -1
+                    val ans = (fir - sec)
+                    tvOutput.text = removeZeros(ans)
+                }
+                // Calculation for addition
+                else if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var fir = splitValue[0].toDouble()
+                    var sec = splitValue[1].toDouble()
+
+                    if (prefix == "-") fir *= -1
+                    val ans = (fir + sec)
+                    tvOutput.text = removeZeros(ans)
+                }
+                // Calculation for multiplication
+                else if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var fir = splitValue[0].toDouble()
+                    var sec = splitValue[1].toDouble()
+
+                    if (prefix == "-") fir *= -1
+                    val ans = (fir * sec)
+                    tvOutput.text = removeZeros(ans)
+                }
+                // Calculation for division
+                else if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var fir = splitValue[0].toDouble()
+                    var sec = splitValue[1].toDouble()
+
+                    if (prefix == "-") fir *= -1
+                    val ans = (fir / sec)
+                    tvOutput.text = removeZeros(ans)
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
     }
 
-//    Creating fun to remove reduntact zeros (For eg: 5.00 -> 5)
-    private fun removeZeros(ans : Double) : String{
-        var intAns = ans.toInt();
+    /**
+     * Removes redundant zeros from the result by converting it to an integer if it is a whole number.
+     */
+    private fun removeZeros(ans: Double): String {
+        val intAns = ans.toInt()
         return if (intAns.toDouble() == ans) intAns.toString() else ans.toString()
     }
 
-//    Checking if the expression has any oprators or not (For eg: 10/2)
-    private fun isOperatorAdded(string : String) : Boolean{
-        if(string.startsWith("-")) return false;
-
-        return ( string.contains("-") || string.contains("+") ||
-            string.contains("*") || string.contains("/") )
+    /**
+     * Checks if an operator is already added to the expression.
+     */
+    private fun isOperatorAdded(string: String): Boolean {
+        if (string.startsWith("-")) return false
+        return (string.contains("-") || string.contains("+") ||
+                string.contains("*") || string.contains("/"))
     }
-
-
 }
